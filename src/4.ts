f1748cdacca3ccd4bbd4a04ca886b536aaf69680
Dmytro Liuba <lyubadmitry2@gmail.com>
2024-02-22 10:12:24 +0200

    update

--- src/4.ts
+++ src/4.ts
@@ -46,9 +46,6 @@ abstract class House {
 }
 
 class MyHouse extends House {
-    constructor(key: Key) {
-        super(key);
-    }
 
     openDoor(key: Key): void {
         if (key.getSignature() === this.key.getSignature()) {
